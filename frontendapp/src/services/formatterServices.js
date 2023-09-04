40ed4dacb253f567b5160e76b2f7a903161b3040
Smitha Kamath <72394054+smitha-2020@users.noreply.github.com>
2023-09-04 11:42:55 +0300

    Add files via upload

---
+++ frontendapp/src/services/formatterServices.js
@@ -0,0 +1,9 @@
+import moment from "moment";
+
+export function getUnixTime(timestamp) {
+  return moment.unix(timestamp).format("dddd, MMMM Do YYYY");
+}
+
+export function getKelvinToCelcius(temp) {
+  return Math.floor(temp - 273.15);
+}
